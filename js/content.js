329234cce8ed6265f7719274e1221fd394297ddc
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2015-05-21 21:50:48 +0300

    Beautify code

--- js/content.js
+++ js/content.js
@@ -2,11 +2,11 @@ var artist;
 var track;
 var pos;
 var lastPos;
-var needScrobble=false;
-var scrobbleEnabled=true;
-var period=1100;
-var periodsToNowPlay  =  20;//число периодов, через которые посылается PlayNow  нотификация
-var periodNum  =0;
+var needScrobble = false;
+var scrobbleEnabled = true;
+var period = 1100;
+var periodsToNowPlay = 20;//число периодов, через которые посылается PlayNow  нотификация
+var periodNum = 0;
 
 
 parseinfo();
@@ -20,153 +20,150 @@ hidded.innerHTML = "<span id=vkScrobblerArtist>vknone</span> <span id=vkScrobble
 document.body.appendChild(hidded);
 
 //исполнить скрипт vk_inner в контексте vk.com
-var script=document.createElement('script');
-script.type='text/javascript';
-script.src=chrome.extension.getURL("js/vk_inner.js");
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = chrome.extension.getURL("js/vk_inner.js");
 
 document.body.appendChild(script);
 
 //вешаем событие на появление мини плеера, чтобы тут же вставить индикаторы
-$("body").on("DOMNodeInserted",function(e){ 
-	if (e.target.id=='pad_wrap'){
-		setTimeout(addIndicators,100);}
+$("body").on("DOMNodeInserted", function (e) {
+  if (e.target.id == 'pad_wrap') {
+    setTimeout(addIndicators, 100);
+  }
 });
 
-function parseinfo()
-{
-	addIndicators();
-	artist = $('#vkScrobblerArtist').text();
-	artist = artist.replace("&amp;", "&");
-	track = $('#vkScrobblerTrackTitle').text();
-	track = track.replace("&amp;", "&");
-	//console.info(artist," - ", track);
-	
-	if ($("#vkScrobblerPos").html() != "vknone")
-		pos = parseFloat( $("#vkScrobblerPos").html() );
-	else pos = 0;
+function parseinfo() {
+  addIndicators();
+  artist = $('#vkScrobblerArtist').text();
+  artist = artist.replace("&amp;", "&");
+  track = $('#vkScrobblerTrackTitle').text();
+  track = track.replace("&amp;", "&");
+  //console.info(artist," - ", track);
 
-	check();
+  if ($("#vkScrobblerPos").html() != "vknone")
+    pos = parseFloat($("#vkScrobblerPos").html());
+  else pos = 0;
 
-	Indicators.setTwitButtonHref( getTwitLink(artist, track) );
+  check();
 
-	setTimeout(parseinfo,period);
+  Indicators.setTwitButtonHref(getTwitLink(artist, track));
+
+  setTimeout(parseinfo, period);
 }
 
-function check()
-{
-	/*if (artist && artist!='vknone'){
-		$("#vkSaveCurrentLinkAC, #vkSaveCurrentLinkPD").show();
-	}*/
-	if (scrobbleEnabled)
-	{
-		if ($("#vkScrobblerNeedScrobble").html()=='true')
-		{
-			needScrobble = true;
-			periodNum = periodsToNowPlay;
-			$("#vkScrobblerNeedScrobble").html('false');
-			//сменился трек
-			trackChanged();
-		}
-		if (needScrobble)	//если ожидается скробблинг, то по достижении пол трека скробблим
-		{
-			if (pos!=lastPos) 
-			{
-				if (periodNum>periodsToNowPlay)
-				{
-					sendNowPlayingRequest();
-					periodNum=0;
-				}
-				Indicators.indicatePlayNow();
-			} else Indicators.indicateVKscrobbler();
+function check() {
+  /*if (artist && artist!='vknone'){
+   $("#vkSaveCurrentLinkAC, #vkSaveCurrentLinkPD").show();
+   }*/
+  if (scrobbleEnabled) {
+    if ($("#vkScrobblerNeedScrobble").html() == 'true') {
+      needScrobble = true;
+      periodNum = periodsToNowPlay;
+      $("#vkScrobblerNeedScrobble").html('false');
+      //сменился трек
+      trackChanged();
+    }
+    if (needScrobble)	//если ожидается скробблинг, то по достижении пол трека скробблим
+    {
+      if (pos != lastPos) {
+        if (periodNum > periodsToNowPlay) {
+          sendNowPlayingRequest();
+          periodNum = 0;
+        }
+        Indicators.indicatePlayNow();
+      } else Indicators.indicateVKscrobbler();
 
-			lastPos = pos;
-			periodNum++;
-			
-			//console.info("Len:"+len+"; pos:"+pos);
-			if (pos >= 50) //отправим после того как половина трека проиграется
-			{
-				sendScrobleRequest();
-				Indicators.indicateScrobbled()
-				needScrobble = false;
-			}	
-		}
-	} else Indicators.indicatePauseScrobbling();
+      lastPos = pos;
+      periodNum++;
+
+      //console.info("Len:"+len+"; pos:"+pos);
+      if (pos >= 50) //отправим после того как половина трека проиграется
+      {
+        sendScrobleRequest();
+        Indicators.indicateScrobbled();
+        needScrobble = false;
+      }
+    }
+  } else Indicators.indicatePauseScrobbling();
 }
 
-function trackChanged(){
-	//сбрасываем индикатор любимой песни
-	Indicators.indicateNotLove();
+function trackChanged() {
+  //сбрасываем индикатор любимой песни
+  Indicators.indicateNotLove();
 }
 
 //add indicators if they not exist
-function addIndicators()
-{
-	Indicators.IncreaseMiniPlayerWidth();
-	
-	Indicators.SetMiniIndicator();
+function addIndicators() {
+  Indicators.IncreaseMiniPlayerWidth();
 
-    //добавляем индикатор к верхней панели на странице аудиозаписей
-    Indicators.SetAllAC();
+  Indicators.SetMiniIndicator();
 
-	//Добавляем индикатор к всплывающему плееру
-	Indicators.SetAllPD();
+  //добавляем индикатор к верхней панели на странице аудиозаписей
+  Indicators.SetAllAC();
 
-	Indicators.indicateStatus();
+  //Добавляем индикатор к всплывающему плееру
+  Indicators.SetAllPD();
+
+  Indicators.indicateStatus();
 }
 
-function togglePauseScrobbling(){
-	scrobbleEnabled = !scrobbleEnabled;
-	if (scrobbleEnabled)
-	{
-		if (pos>50)
-			Indicators.indicateScrobbled();
-		else Indicators.indicateVKscrobbler();
-	} else {
-		Indicators.indicatePauseScrobbling();
-	}
-	sendPauseStatus(!scrobbleEnabled);
+function togglePauseScrobbling() {
+  scrobbleEnabled = !scrobbleEnabled;
+  if (scrobbleEnabled) {
+    if (pos > 50)
+      Indicators.indicateScrobbled();
+    else Indicators.indicateVKscrobbler();
+  } else {
+    Indicators.indicatePauseScrobbling();
+  }
+  sendPauseStatus(!scrobbleEnabled);
 }
 /*function saveCurrentTrack(e){
-	if (!artist || artist=='vknone'){
-		e.stopPropagation();
-		return false;
-	}
-	var fileName = artist+" - "+track + ".mp3";
-	var href = $('#saveCurrentHref').text();
-	$(this).attr("href", href+"/"+fileName);
-	$(this).attr("download", fileName);
+ if (!artist || artist=='vknone'){
+ e.stopPropagation();
+ return false;
+ }
+ var fileName = artist+" - "+track + ".mp3";
+ var href = $('#saveCurrentHref').text();
+ $(this).attr("href", href+"/"+fileName);
+ $(this).attr("download", fileName);
 
-}*/
+ }*/
 
-function getTwitLink(artist, track)
-{
-	if (artist == "vknone") return;
-	var twtLink = 'http://twitter.com/home?status='+encodeURIComponent("#nowplaying")+" "+
-	 encodeURIComponent(artist) +" - "+ encodeURIComponent(track) + encodeURIComponent(" via #vkscrobbler") + " http://bit.ly/yQg0uN";
-	return twtLink;
+function getTwitLink(artist, track) {
+  if (artist == "vknone") return;
+  var twtLink = 'http://twitter.com/home?status=' + encodeURIComponent("#nowplaying") + " " +
+    encodeURIComponent(artist) + " - " + encodeURIComponent(track) + encodeURIComponent(" via #vkscrobbler") + " http://bit.ly/yQg0uN";
+  return twtLink;
 }
 
-function sendScrobleRequest()
-{
-	if (artist!="vknone" && title!="vknone")
-		chrome.extension.sendRequest({message: "needScrobble", artist: artist, title: track}, function(obj) { });
-	else console.info("none artist detected");
+function sendScrobleRequest() {
+  if (artist != "vknone" && title != "vknone")
+    chrome.extension.sendRequest({message: "needScrobble", artist: artist, title: track}, function (obj) {
+    });
+  else console.info("none artist detected");
 }
 
-function sendNowPlayingRequest()
-{
-	if (artist!="vknone" && title!="vknone")
-		chrome.extension.sendRequest({message: "nowPlaying", artist: artist, title: track}, function(obj) { });
-	else console.info("none artist detected");
+function sendNowPlayingRequest() {
+  if (artist != "vknone" && title != "vknone")
+    chrome.extension.sendRequest({message: "nowPlaying", artist: artist, title: track}, function (obj) {
+    });
+  else console.info("none artist detected");
 }
 
-function sendLoveRequest()
-{
-	chrome.extension.sendRequest({message: "needLove", artist: artist, title: track}, function(obj) { });
-	Indicators.indicateLoved();
+function sendLoveRequest() {
+  chrome.extension.sendRequest({message: "needLove", artist: artist, title: track}, function (obj) {
+  });
+  Indicators.indicateLoved();
 }
 
-function sendPauseStatus(paused)
-{
-	chrome.extension.sendRequest({message: "togglePauseScrobbling", paused: paused, artist: artist, title: track}, function(obj) { });
+function sendPauseStatus(paused) {
+  chrome.extension.sendRequest({
+    message: "togglePauseScrobbling",
+    paused: paused,
+    artist: artist,
+    title: track
+  }, function (obj) {
+  });
 }
--- js/vk_inner.js
+++ js/vk_inner.js
@@ -1,147 +1,144 @@
-﻿
-var timerTryer = setInterval(tryToAttachScrobblerListeners,1000);
+﻿var timerTryer = setInterval(tryToAttachScrobblerListeners, 1000);
 
 
-function tryToAttachScrobblerListeners()
-{
-	if (typeof audioPlayer!='undefined'){
-	
-		var CURRENT_POS = 0;
+function tryToAttachScrobblerListeners() {
+  if (typeof audioPlayer != 'undefined') {
 
-		var ARTIST_NUM = 5;
-		var TITLE_NUM = 6;
-		var SAVE_LINK = 2;
+    var CURRENT_POS = 0;
 
-		var playedTime=0;
+    var ARTIST_NUM = 5;
+    var TITLE_NUM = 6;
+    var SAVE_LINK = 2;
 
-		//вызывается ли прямо сейчас метод audioPlayer.operate
-		var IsPlayerOperating = false;
+    var playedTime = 0;
 
-		var posElem = document.getElementById('vkScrobblerPos');
-		var artistElem = document.getElementById('vkScrobblerArtist');
-		var titleElem = document.getElementById('vkScrobblerTrackTitle');
-		var needScrobbleElem = document.getElementById('vkScrobblerNeedScrobble');
-		var saveCurrentLinkElem = document.getElementById('saveCurrentHref');
+    //вызывается ли прямо сейчас метод audioPlayer.operate
+    var IsPlayerOperating = false;
 
-		//вешаем слушатель переключения песен
-		audioPlayer.operate = Function.addCallListener(audioPlayer.operate, {
-		    before: function(props) {
-		    	//console.log('before operate', props);
-		    	IsPlayerOperating = true;
-		    },
-		    after: function(props){
-		    	IsPlayerOperating = false;
-		    }
-		});
+    var posElem = document.getElementById('vkScrobblerPos');
+    var artistElem = document.getElementById('vkScrobblerArtist');
+    var titleElem = document.getElementById('vkScrobblerTrackTitle');
+    var needScrobbleElem = document.getElementById('vkScrobblerNeedScrobble');
+    var saveCurrentLinkElem = document.getElementById('saveCurrentHref');
 
-		//вешаем слушатель начала новой песни
-		audioPlayer.loadGlobal = Function.addCallListener(audioPlayer.loadGlobal, {
-		    after: function(props) {
-		    	//слушаем только те loadGlobal, которые вызываются метоом operate
-		    	if (!IsPlayerOperating) return ;
-		    	IsPlayerOperating = false;
+    //вешаем слушатель переключения песен
+    audioPlayer.operate = Function.addCallListener(audioPlayer.operate, {
+      before: function (props) {
+        //console.log('before operate', props);
+        IsPlayerOperating = true;
+      },
+      after: function (props) {
+        IsPlayerOperating = false;
+      }
+    });
 
-		    	playedTime = 0;
+    //вешаем слушатель начала новой песни
+    audioPlayer.loadGlobal = Function.addCallListener(audioPlayer.loadGlobal, {
+      after: function (props) {
+        //слушаем только те loadGlobal, которые вызываются метоом operate
+        if (!IsPlayerOperating) return;
+        IsPlayerOperating = false;
 
-		    	var lastVkScrobblerPos = posElem.innerHTML;
+        playedTime = 0;
 
-		    	//ждём пока позиция изменится и только тогда говорим, что пора скробблить (чтобы не заскробблить текущую песню дважды)
-		    	var timeout = setInterval(function(){
-		    		if (posElem.innerHTML != lastVkScrobblerPos)
-		    		{
-		    			needScrobbleElem.innerHTML='true'; 
-		    			clearInterval(timeout);
-		    		}
-		    	}, 1000);
-		      //console.log('after loadGlobal', props);
-		    }
-		});
+        var lastVkScrobblerPos = posElem.innerHTML;
 
-		//вешаем слушатель прогресса песни
-		audioPlayer.onPlayProgress = Function.addCallListener(audioPlayer.onPlayProgress, {
-			after: function(props) {
-				//сохраняем исполнителя и песню
-				artistElem.innerHTML = audioPlayer.lastSong[ ARTIST_NUM ];
-				titleElem.innerHTML = audioPlayer.lastSong[ TITLE_NUM ];
+        //ждём пока позиция изменится и только тогда говорим, что пора скробблить (чтобы не заскробблить текущую песню дважды)
+        var timeout = setInterval(function () {
+          if (posElem.innerHTML != lastVkScrobblerPos) {
+            needScrobbleElem.innerHTML = 'true';
+            clearInterval(timeout);
+          }
+        }, 1000);
+        //console.log('after loadGlobal', props);
+      }
+    });
 
-				saveCurrentLinkElem.innerHTML = audioPlayer.lastSong[ SAVE_LINK ];
+    //вешаем слушатель прогресса песни
+    audioPlayer.onPlayProgress = Function.addCallListener(audioPlayer.onPlayProgress, {
+      after: function (props) {
+        //сохраняем исполнителя и песню
+        artistElem.innerHTML = audioPlayer.lastSong[ARTIST_NUM];
+        titleElem.innerHTML = audioPlayer.lastSong[TITLE_NUM];
 
-				if(needScrobbleElem.innerHTML=='vknone')
-					needScrobbleElem.innerHTML='true';
-				
-			}
-		});
+        saveCurrentLinkElem.innerHTML = audioPlayer.lastSong[SAVE_LINK];
 
-		var timeStamp=0;
+        if (needScrobbleElem.innerHTML == 'vknone')
+          needScrobbleElem.innerHTML = 'true';
 
-		setInterval(function() {
-			if (playedTime==0){
-	    		timeStamp=new Date(new Date()-1000);
-	    	}
+      }
+    });
 
-	        var timeDiff = new Date() - timeStamp;
-	        timeStamp = new Date();
+    var timeStamp = 0;
 
-		    if (currentAudioId() && !audioPlayer.player.paused()) {
-		    	
-		    	//сохраняем проигранного времени в песне
-		    	playedTime += timeDiff/1000;
+    setInterval(function () {
+        if (playedTime == 0) {
+          timeStamp = new Date(new Date() - 1000);
+        }
 
-		    	playedPercent = playedTime / audioPlayer.duration * 100;
-		    	if (playedPercent == Infinity) curPos = 0;
-		        posElem.innerHTML = playedPercent;
+        var timeDiff = new Date() - timeStamp;
+        timeStamp = new Date();
 
-		    	/*console.log("log",{
-		    		playedTime: playedTime,
-		    		duration: audioPlayer.duration,
-		    		playedPercent: playedPercent,
-		    		timeDiff: timeDiff,
-		    		timeStamp: timeStamp,
-		    	});*/
-		    }
-		}
-		, 1000);
+        if (currentAudioId() && !audioPlayer.player.paused()) {
 
-		//отключаем попытки навесить слушатели
-		clearInterval(timerTryer);
+          //сохраняем проигранного времени в песне
+          playedTime += timeDiff / 1000;
 
-	}
+          playedPercent = playedTime / audioPlayer.duration * 100;
+          if (playedPercent == Infinity) curPos = 0;
+          posElem.innerHTML = playedPercent;
+
+          /*console.log("log",{
+           playedTime: playedTime,
+           duration: audioPlayer.duration,
+           playedPercent: playedPercent,
+           timeDiff: timeDiff,
+           timeStamp: timeStamp,
+           });*/
+        }
+      }
+      , 1000);
+
+    //отключаем попытки навесить слушатели
+    clearInterval(timerTryer);
+
+  }
 }
 
 //добавляем прототип слушатель
-Function.addCallListener = function(func, callbacks) {
-    var successNumber = 0,
-        errorNumber = 0,
-        name = func.name;
+Function.addCallListener = function (func, callbacks) {
+  var successNumber = 0,
+    errorNumber = 0,
+    name = func.name;
 
-    return function() {
-        var args = [].slice.call(arguments);
-        var result, error;
+  return function () {
+    var args = [].slice.call(arguments);
+    var result, error;
 
-        var props = {
-            args: args,
-            self: this,
-            name: name
-        }
-
-
-        callbacks.before && callbacks.before(props);
-
-        try {
-            result = func.apply(this, arguments);
-            props.successNumber = ++successNumber;
-            props.result = result;
-            props.status = 'success';
-            callbacks.success && callbacks.success(props);
-        } catch (e) {
-            props.errorNumber = ++errorNumber;
-            props.error = e;
-            props.status = 'error';
-            callbacks.error && callbacks.error(props);
-        }
-
-        callbacks.after && callbacks.after(props);
-
-        return result;
+    var props = {
+      args: args,
+      self: this,
+      name: name
     }
-}
+
+
+    callbacks.before && callbacks.before(props);
+
+    try {
+      result = func.apply(this, arguments);
+      props.successNumber = ++successNumber;
+      props.result = result;
+      props.status = 'success';
+      callbacks.success && callbacks.success(props);
+    } catch (e) {
+      props.errorNumber = ++errorNumber;
+      props.error = e;
+      props.status = 'error';
+      callbacks.error && callbacks.error(props);
+    }
+
+    callbacks.after && callbacks.after(props);
+
+    return result;
+  }
+};
