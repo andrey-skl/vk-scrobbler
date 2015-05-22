51825b206070ead0b52afc0eb750e08d2f32e1b1
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2015-05-22 22:40:32 +0300

    Use md5 implementation from NPM

--- copy.sh
+++ copy.sh
@@ -3,4 +3,5 @@
 cp -rv ./blocks dist/blocks
 cp -rv ./_locales dist/_locales
 cp -rv ./img dist/img
+cp node_modules/js-md5/build/md5.min.js dist/node_modules/js-md5/build/md5.min.js
 cp manifest.json dist/
