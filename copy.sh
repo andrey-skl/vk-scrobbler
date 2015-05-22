263d3927b4477ccc98a52eafc08dffb2dd2146c2
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2015-05-22 22:54:39 +0300

    FIx build

--- copy.sh
+++ copy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-cp -rv ./blocks dist/blocks
-cp -rv ./_locales dist/_locales
-cp -rv ./img dist/img
+cp -rv -X ./blocks dist/blocks
+cp -rv -X ./_locales dist/_locales
+cp -rv -X ./img dist/img
+mkdir -p dist/node_modules/js-md5/build
 cp node_modules/js-md5/build/md5.min.js dist/node_modules/js-md5/build/md5.min.js
 cp manifest.json dist/
