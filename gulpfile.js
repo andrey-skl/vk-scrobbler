e4ac40786f517f469fd8ade803dcc55db3b33f22
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2015-05-23 22:57:04 +0300

    Fix mixed manifest

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,22 @@ var gulp = require('gulp');
 var watch = require('gulp-watch');
 
 var source = './*(_locales|blocks|img)/**/**!(test|foo).*';
-var notTests = '!./**/*.test.js';
+var manifest = './manifest.json';
 var components = './node_modules/js-md5/build/*';
+var notTests = '!./**/*.test.js';
+
+var gulpSources = [manifest, source, components, notTests];
+
 var destination = './dist';
 
 gulp.task('watch-copy', function() {
-  return gulp.src([source, components, notTests], {base: './'})
-    .pipe(watch(source))
+  return gulp.src(gulpSources, {base: './'})
+    .pipe(watch(gulpSources))
     .pipe(gulp.dest(destination));
 });
 
 gulp.task('copy', function() {
   gulp
-    .src([source, components, notTests], {base: './'})
+    .src(gulpSources, {base: './'})
     .pipe(gulp.dest(destination));
 });
