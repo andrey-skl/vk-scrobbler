1dee9f6fdac50450c3cadaaba8d7a392ee6ab98b
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2015-05-23 19:19:49 +0300

    Tests for lastfm API requests

--- blocks/lastfm-api/lastfm-api.test.js
+++ blocks/lastfm-api/lastfm-api.test.js
@@ -4,7 +4,8 @@ describe('API client', function () {
   var fakeParams = {
     artist: 'Foo',
     title: 'Bar',
-    key: 'key'
+    key: 'key',
+    userName: 'testUser'
   };
 
   beforeEach(function () {
@@ -61,6 +62,108 @@ describe('API client', function () {
     it('Should send timestamp', function () {
       var timeStamp = Math.round(new Date().getTime() / 1000);
       this.getLastRequest().requestBody.should.contain('timestamp=' + timeStamp);
+    });
+  });
+
+  describe('Now playing request', function () {
+
+    beforeEach(function () {
+      api.nowPlaying(fakeParams);
+    });
+
+    it('Should send correct method', function () {
+      this.getLastRequest().requestBody.should.contain('method=track.updateNowPlaying');
+    });
+
+    it('Should send artist', function () {
+      this.getLastRequest().requestBody.should.contain('artist=Foo');
+    });
+
+    it('Should send track', function () {
+      this.getLastRequest().requestBody.should.contain('track=Bar');
+    });
+
+    it('Should send api key', function () {
+      this.getLastRequest().requestBody.should.contain('api_key=' + window.LastFmApiConfig.apiKey);
+    });
+  });
+
+  describe('makeLoved request', function () {
+
+    beforeEach(function () {
+      api.makeLoved(fakeParams);
+    });
+
+    it('Should send correct method', function () {
+      this.getLastRequest().requestBody.should.contain('track.love');
+    });
+
+    it('Should send artist', function () {
+      this.getLastRequest().requestBody.should.contain('artist=Foo');
+    });
+
+    it('Should send track', function () {
+      this.getLastRequest().requestBody.should.contain('track=Bar');
+    });
+  });
+
+  describe('makeNotLoved request', function () {
+
+    beforeEach(function () {
+      api.makeNotLoved(fakeParams);
+    });
+
+    it('Should send correct method', function () {
+      this.getLastRequest().requestBody.should.contain('track.unlove');
+    });
+
+    it('Should send artist', function () {
+      this.getLastRequest().requestBody.should.contain('artist=Foo');
+    });
+
+    it('Should send track', function () {
+      this.getLastRequest().requestBody.should.contain('track=Bar');
+    });
+  });
+
+  describe('getTrackInfo request', function () {
+    var promise;
+
+    beforeEach(function () {
+      promise = api.getTrackInfo(fakeParams);
+    });
+
+    it('Should send correct method', function () {
+      this.getLastRequest().requestBody.should.contain('track.getInfo');
+    });
+
+    it('Should send artist', function () {
+      this.getLastRequest().requestBody.should.contain('artist=Foo');
+    });
+
+    it('Should send track', function () {
+      this.getLastRequest().requestBody.should.contain('track=Bar');
+    });
+
+    it('Should send username', function () {
+      this.getLastRequest().requestBody.should.contain('username=testUser');
+    });
+
+    it('Should return response', function (done) {
+      var fakeResponse = {
+        track: {
+          artist: 'testArtist',
+          userloved: '1'
+        }
+      };
+
+      promise.then(function (result) {
+        result.track.artist.should.be.equal('testArtist');
+        result.track.userloved.should.be.equal('1');
+        done();
+      });
+
+      this.getLastRequest().respond(200, { "Content-Type": "application/json" }, JSON.stringify(fakeResponse));
     });
   });
 
