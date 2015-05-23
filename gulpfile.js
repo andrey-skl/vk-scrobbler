8fa14376bd309572482e250b4aa871c00b6ec1cb
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2015-05-23 22:42:57 +0300

    Exclude tests from build

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 
-var source = './*(_locales|blocks|img)/**/*';
+var source = './*(_locales|blocks|img)/**/**!(test|foo).*';
+var notTests = '!./**/*.test.js';
 var components = './node_modules/js-md5/build/*';
 var destination = './dist';
 
 gulp.task('watch-copy', function() {
-  return gulp.src([source, components], {base: './'})
+  return gulp.src([source, components, notTests], {base: './'})
     .pipe(watch(source))
     .pipe(gulp.dest(destination));
 });
 
 gulp.task('copy', function() {
   gulp
-    .src([source, components], {base: './'})
+    .src([source, components, notTests], {base: './'})
     .pipe(gulp.dest(destination));
 });
