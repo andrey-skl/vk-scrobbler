43fd51620788b5bcf7835a173be91542015a3ae3
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2015-05-30 22:53:11 +0300

    Refactoring for methods monkey patcher

--- blocks/vk-inner/vk-inner__player.js
+++ blocks/vk-inner/vk-inner__player.js
@@ -9,5 +9,24 @@
     chrome.runtime.sendMessage(this.extensionId, msg);
   };
 
+  /**
+   * Adds listener to function's calls by monkey patching
+   * @param func - function to listen
+   * @param callbacks, can contain "after" and "before" callbacks
+   * @returns {Function} - patched function
+   */
+  PlayerPatcher.addCallListener = function (object, method, callbacks) {
+    var original = object[method];
+
+    object[method] = function callHandler() {
+      callbacks.before && callbacks.before.apply(callbacks, arguments);
+
+      var result = original.apply(this, arguments);
+
+      callbacks.after && callbacks.after.apply(callbacks, arguments);
+      return result;
+    };
+  };
+
   window.vkScrobbler.PlayerPatcher = PlayerPatcher;
 })();
--- blocks/vk-inner/vk-inner__player.test.js
+++ blocks/vk-inner/vk-inner__player.test.js
@@ -19,4 +19,34 @@ describe('Vk-inner player', function () {
     patcher.sendMessage({foo: 'bar'});
     chrome.runtime.sendMessage.should.have.been.calledWith('fakeId', {foo: 'bar'});
   });
+
+  describe('callListener', function () {
+    var method;
+    var handler;
+    var fakeObj = {};
+
+    beforeEach(function () {
+      method = sinon.stub();
+      fakeObj.method = method;
+    });
+
+    beforeEach(function () {
+      handler = {
+        after: sinon.stub()
+      };
+      PlayerPatcher.addCallListener(fakeObj, 'method', handler);
+    });
+
+    it('Should pass call to original', function () {
+      fakeObj.method('foo', {bar: 'test'});
+
+      method.should.have.been.calledWith('foo', {bar: 'test'});
+    });
+
+    it('Should call "after" handler', function () {
+      fakeObj.method('foo', {bar: 'test'});
+
+      handler.after.should.have.been.calledWith('foo', {bar: 'test'});
+    });
+  });
 });
