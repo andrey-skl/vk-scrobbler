b17e216483340b839a0a0d36f72da319f2aca372
stsdc <stanislaw.dac@gmail.com>
2016-05-02 23:24:02 +0200

    auth works

--- blocks/background/background__auth.js
+++ blocks/background/background__auth.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
   'use strict';
 
   var LastFmApiConfig = window.vkScrobbler.LastFmApiConfig;
@@ -13,27 +13,34 @@
   }
 
   function redirectToAuthPage(tabId, urlSearch) {
-    chrome.tabs.update(tabId, {url: "blocks/auth/auth.html" + urlSearch, active: true}, null);
+    chrome.tabs.update(tabId, {
+      url: "blocks/auth/auth.html" + urlSearch,
+      active: true
+    }, null);
   }
 
-  var backgroundAuth = function () {
+  var backgroundAuth = function() {
 
     openLastFmAuthTab();
 
     function handleOAuthRedirect() {
+          // @TODO
+          // I think, It should be rewritten
+      chrome.tabs.query({
+          active: true,
+          currentWindow: true
+      },
+        function(tabs) {
+          for (var i = 0; i < tabs.length; i++) {
+            var tabUrl = tabs[i].url;
 
-      chrome.tabs.getAllInWindow(null, function (tabs) {
-        for (var i = 0; i < tabs.length; i++) {
-          var tabUrl = tabs[i].url;
-
-          if (tabUrl.indexOf(oauthRedirectUrl) === 0) {
-            chrome.tabs.onUpdated.removeListener(handleOAuthRedirect);
-            redirectToAuthPage(tabs[i].id, tabUrl.match(tokenRegEx));
-            return;
+            if (tabUrl.indexOf(oauthRedirectUrl) === 0) {
+              chrome.tabs.onUpdated.removeListener(handleOAuthRedirect);
+              redirectToAuthPage(tabs[i].id, tabUrl.match(tokenRegEx));
+              return;
+            }
           }
-        }
-      });
-
+        });
     }
 
     chrome.tabs.onUpdated.addListener(handleOAuthRedirect);
