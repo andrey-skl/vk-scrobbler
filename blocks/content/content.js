252963ce5dee03b615c9bddd700957063e0ad20b
stsdc <stanislaw.dac@gmail.com>
2016-07-05 19:24:14 +0200

    replaced Mutation events with MutationObserver in old ui

--- blocks/content/content.js
+++ blocks/content/content.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
   'use strict';
 
   var Indicators = window.vkScrobbler.Indicators;
@@ -8,9 +8,9 @@
   var isOldUI = Boolean(document.getElementById('top_new_msg')); //new UI doesn't have this element
   console.info('vk scrobbler. New ui detected = ', !isOldUI);
 
-  function instantIndicatorsInserter () {
+  function instantIndicatorsInserter() {
     //listen to players inserting in document to instantly insert indicators nodes
-    document.body.addEventListener('DOMNodeInserted', function (e) {
+    document.body.addEventListener('DOMNodeInserted', function(e) {
       if (e.target.classList && e.target.classList.contains('audio_page_player_volume_slider')) {
         Indicators.SetDropdownIndicators();
         Indicators.SetAudioPageIndicators();
@@ -22,34 +22,48 @@
     Indicators.SetAudioPageIndicators();
     Indicators.SetHeaderIndicator();
 
-    window.addEventListener('load', function () {
+    window.addEventListener('load', function() {
       Indicators.SetAudioPageIndicators();
       Indicators.SetHeaderIndicator();
     });
   }
 
-  function instantIndicatorsInserterOld () {
-    //listen to players inserting in document to instantly insert indicators nodes
-    document.body.addEventListener('DOMNodeInserted', function (e) {
-      if (e.target.id === 'pad_controls') {
-        IndicatorsOld.SetAllPD();
-      }
-      if (e.target.classList && e.target.classList.contains('wrap') && e.target.querySelector('#gp_info')) {
-        IndicatorsOld.SetAllMini();
-      }
-      if(e.target.id === 'wrap2' && e.target.querySelector('#audio') !== null) {
-        IndicatorsOld.SetAllAC();
-      }
-    });
+  function instantIndicatorsInserterOld() {
+    //observe players inserting in document to instantly insert indicators nodes
+
+    // http://frontender.info/mutation-observers-tutorial
+
+    var playersObserver = new MutationObserver(function(mutations) {
+        mutations.map(function(mutation) {
+          // console.log("id:"+mutation.target.id);
+          if (mutation.target.id === 'pad_cont') {
+            IndicatorsOld.SetAllPD();
+            console.log("[vk scrobbler] Indicators inserted in the old music pad.");
+          }
+          if (mutation.target.id === 'gp') {
+            IndicatorsOld.SetAllMini();
+            console.log("[vk scrobbler] Indicators inserted in the old music left minipad.");
+          }
+          if (mutation.target.id === 'wrap3' && mutation.target.querySelector('#audio') !== null) {
+            IndicatorsOld.SetAllAC();
+            console.log("[vk scrobbler] Indicators inserted in the old music page topbar.");
+          }
+        });
+      }),
+      options = {
+        'childList': true,
+        'subtree': true
+      };
+    playersObserver.observe(document.body, options);
 
     //If audio page is a landing page, then just attaching indicators
     IndicatorsOld.SetAllAC();
-    window.addEventListener("load", function () {
+    window.addEventListener("load", function() {
       IndicatorsOld.SetAllAC();
     });
   }
 
-  var activate = function () {
+  var activate = function() {
     if (isOldUI) {
       instantIndicatorsInserterOld();
     } else {
@@ -58,7 +72,7 @@
 
     scriptInjector.injectPatcher();
 
-    window.addEventListener('message', function(e){
+    window.addEventListener('message', function(e) {
       if (e.data.vkPlayerPatcherMessage) {
         var payload = e.data.message;
 
