4710e75e73eb945f7abdb2f8182ce9123e44ce9e
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2016-08-01 21:14:44 +0300

    #18 Improve google analytics collecting code

--- blocks/analytics/analytics.js
+++ blocks/analytics/analytics.js
@@ -1,29 +1,29 @@
 (function () {
   const GA_TRACKING_ID = 'UA-37330826-2';
-  _ga = function(type, args) {
-	  try {
-      let message = "v=1&tid=" + GA_TRACKING_ID + "&aip=1&ds=add-on" +
-          '&t=' + type;
-      let arg_names = [];
-      if (type == 'event')
-        arg_names = ['ec', 'ea', 'el', 'ev'];
-      else if (type == 'pageview')
-        arg_names = ['dp'];
-      for (int i = 0; i < arg_names.length; ++i)
-        if (args[i])
-          message += '&' + arg_names[i] = '=' + encodeURIComponent(''+args[i]);
-		  let request = new XMLHttpRequest();
-		  request.open("POST", "https://www.google-analytics.com/collect", true);
-		  request.send(message);
-	  } catch (e) {
-		  console.log("Error sending report to Google Analytics.\n" + e);
-	  }
-  }
-  _ga('pageview');
+  const GA_URL = 'https://www.google-analytics.com/collect';
+  window._ga = function(type, ...args) {
+    let message = `v=1&tid=${GA_TRACKING_ID}&aip=1&ds=add-on&t=${type}`;
+
+    let argumentNames = type === 'event' ? ['ec', 'ea', 'el', 'ev'] : ['dp'];
+
+    message = message + args
+        .map((value, index) => `${argumentNames[index]}=${encodeURIComponent(value)}`)
+        .join('&');
+
+    return window.fetch(GA_URL, {
+      method: 'POST',
+      body: message
+    })
+      .catch((e) => {
+        console.error("Error sending report to Google Analytics.\n" + e);
+      });
+  };
+
+  window._ga('pageview');
 })();
 
 window.onerror = function (msg, url, line) {
   var preventErrorAlert = true;
-  _ga('event', 'JS Error', msg, navigator.userAgent + ' -> ' + url + " : " + line, 0, true);
+  window._ga('event', 'JS Error', msg, navigator.userAgent + ' -> ' + url + " : " + line, 0, true);
   return preventErrorAlert;
 };
